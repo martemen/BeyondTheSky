9ff78a2b077a7c003d84c8eec4b826ce91aef104
martemen <martemen@outlook.com>
2023-09-15 21:41:11 +0300

    Added Valkyrien Skies API.

--- src/main/kotlin/net/grumpyaaa/beyondthesky/BeyondTheSky.kt
+++ src/main/kotlin/net/grumpyaaa/beyondthesky/BeyondTheSky.kt
@@ -4,8 +4,8 @@ import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
 object BeyondTheSky : ModInitializer {
-	public val modId: String = "beyondthesky";
-    private val logger = LoggerFactory.getLogger("beyondthesky")
+	val modId: String = "beyondthesky";
+    val logger = LoggerFactory.getLogger("beyondthesky")
 
 	override fun onInitialize() {
 		logger.info("Hello Fabric world! From, $modId")
